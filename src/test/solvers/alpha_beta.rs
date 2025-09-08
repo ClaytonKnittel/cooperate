0f45516e1d8cfbd5b6038a5e110f761b74d64904
Clayton Knittel <cknit1999@gmail.com>
2025-09-07 23:51:42 -0700

    Simplifid

--- src/test/solvers/alpha_beta.rs
+++ src/test/solvers/alpha_beta.rs
@@ -1,20 +1,4 @@
-use std::ops::ControlFlow;
-
 use abstract_game::{Game, GameResult, Score, Solver};
-
-/// A temporary workaround until https://github.com/rust-lang/rust/issues/137461
-/// is stabilized.
-trait ControlFlowIntoValue<T> {
-  fn custom_into_value(self) -> T;
-}
-
-impl<T> ControlFlowIntoValue<T> for ControlFlow<T, T> {
-  fn custom_into_value(self) -> T {
-    match self {
-      ControlFlow::Continue(t) | ControlFlow::Break(t) => t,
-    }
-  }
-}
 
 pub struct AlphaBeta;
 
@@ -35,29 +19,23 @@ impl AlphaBeta {
     }
   }
 
-  fn solve_impl<G: Game>(game: &G, depth: u32, mut alpha: Score, beta: Score) -> Score {
+  fn solve_impl<G: Game>(game: &G, depth: u32, alpha: Score, beta: Score) -> Score {
     debug_assert!(matches!(game.finished(), GameResult::NotFinished));
     debug_assert!(alpha <= beta);
     if depth == 0 {
       return Score::no_info();
     }
 
-    game
-      .each_move()
-      .map(|m| game.with_move(m))
-      .map(|next_game| {
-        let score = Self::score_for_game(&next_game, depth, alpha, beta);
-        alpha = alpha.max(score);
-        score
-      })
-      .try_fold(Score::lose(1), |best_score, score| {
-        if score > beta {
-          ControlFlow::Break(score)
-        } else {
-          ControlFlow::Continue(score.max(best_score))
-        }
-      })
-      .custom_into_value()
+    let mut best_score = Score::lose(1);
+    for next_game in game.each_move().map(|m| game.with_move(m)) {
+      let score = Self::score_for_game(&next_game, depth, alpha.max(best_score), beta);
+      best_score = best_score.max(score);
+      if score > beta {
+        break;
+      }
+    }
+
+    best_score
   }
 }
 
